bec0bcde50601fea7497641f92b49c97b13bcea0
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2023-12-18 19:41:07 +1100

    nextjs v14

---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				dark: {
+					primary: '#661AE6',
+					secondary: '#D926AA',
+					accent: '#1FB2A5',
+					neutral: '#191D24',
+					'base-100': '#2A303C',
+					info: '#3ABFF8',
+					success: '#36D399',
+					warning: '#FBBD23',
+					error: '#F87272',
+				},
+			},
+		],
+	},
+};
+export default config;
