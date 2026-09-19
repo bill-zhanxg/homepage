fee19f32dde30b932bda3b305c95a1ea2ad69d65
Bill <76557923+bill-zhanxg@users.noreply.github.com>
2026-09-19 10:23:14 +1000

    fix: make it look better

--- app/layout.tsx
+++ app/layout.tsx
@@ -105,6 +105,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 												label: 'Cool Stuff',
 												emoji: { name: '😎' },
 											},
+										],
+									},
+									{
+										type: 1,
+										components: [
 											{
 												type: 2,
 												style: 5,
@@ -112,11 +117,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 												label: 'Discord',
 												emoji: { name: '💬' },
 											},
-										],
-									},
-									{
-										type: 1,
-										components: [
 											{
 												type: 2,
 												style: 5,
